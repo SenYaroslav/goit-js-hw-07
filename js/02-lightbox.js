e4d80d7100927f42440e19c2ec874c7bb198768e
Iaroslav Seniuk <savuk7@icloud.com>
2022-10-26 14:31:17 +0300

    додає розмітку 2 завдання

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainerRef = document.querySelector(".gallery");
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryContainerRef.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+
+function createGalleryItemsMarkup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+         <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    `;
+    })
+    .join("");
+}
